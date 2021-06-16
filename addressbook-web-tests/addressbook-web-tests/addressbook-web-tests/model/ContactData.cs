9f4f134418146e99b758fef6db947bbc989e659a
Sukhar <ssuxxarr@gmail.com>
2021-06-16 19:38:38 +0300

    Add REgexp to cleanUp method. Add missimng fixture to ContactInformationTest

--- addressbook-web-tests/addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace WebAddressbookTests
 {
@@ -68,7 +69,7 @@ namespace WebAddressbookTests
             {
                 return null;
             }
-            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+            return Regex.Replace(phone, "[ -()]", "\r\n");
         }
 
         public string Name { get; set; }
